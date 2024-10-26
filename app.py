c517e00e4167084c10414602a4339dd24955ed29
Jedidah Mwaura <139024905+JedidahWM@users.noreply.github.com>
2024-10-25 23:24:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ def generate_qr():
     return render_template('index.html', data=img_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
